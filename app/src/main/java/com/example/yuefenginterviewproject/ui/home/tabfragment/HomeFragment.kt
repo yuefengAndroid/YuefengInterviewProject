b6d55f65d015f58adb3807a1861509fcb8497a04
Yuefeng <yuefeng0129@gmail.com>
2025-02-02 00:24:39 +0800

    [20240127] 首頁HomeFragment環境建置V2

--- app/src/main/java/com/example/yuefenginterviewproject/ui/home/tabfragment/HomeFragment.kt
+++ app/src/main/java/com/example/yuefenginterviewproject/ui/home/tabfragment/HomeFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.yuefenginterviewproject.R
 import com.example.yuefenginterviewproject.data.model.HomeViewModel
 import com.example.yuefenginterviewproject.databinding.FragmentHomeBinding
 
@@ -28,6 +31,17 @@ class HomeFragment : Fragment() {
 
         observeData()  // 監聽 LiveData
         setupRecyclerView() // 初始化 RecyclerView
+
+        val radioButtons = listOf(binding.rbTime, binding.rbGlobal, binding.rbShop)
+
+        radioButtons.forEach { button ->
+            button.setOnClickListener {
+                radioButtons.forEach { it.isChecked = false } // 先把全部設為 false
+                button.isChecked = true // 只讓當前點擊的為 true
+                setButtonStatus(button)
+            }
+        }
+
         return binding.root
     }
 
@@ -50,6 +64,39 @@ class HomeFragment : Fragment() {
             layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false) // 設置為橫向
             adapter = this@HomeFragment.adapter
             overScrollMode = View.OVER_SCROLL_NEVER // 防止滾動時出現反彈效果
+        }
+    }
+
+    // 設定按鈕狀態
+    private fun setButtonStatus(selectedButton: RadioButton) {
+        when (selectedButton.id) {
+            R.id.rb_time -> {
+                binding.rbTime.translationZ = 2f
+                binding.rbGlobal.translationZ = 1f
+                binding.rbShop.translationZ = 0f
+                binding.viewBg.translationZ = 1f
+
+                binding.viewBg.setBackgroundResource(R.drawable.bg_home_hour_sale_bottom_active)
+                binding.timerSeconds.setTextColor(ContextCompat.getColor(requireContext(), R.color.color_cc1e05))
+            }
+            R.id.rb_global -> {
+                binding.rbTime.translationZ = 0f
+                binding.rbGlobal.translationZ = 1f
+                binding.rbShop.translationZ = 0f
+                binding.viewBg.translationZ = 0f
+
+                binding.viewBg.setBackgroundResource(R.drawable.bg_home_global_hoursale_bottom_active)
+                binding.timerSeconds.setTextColor(ContextCompat.getColor(requireContext(), R.color.color_fe5a0a))
+            }
+            R.id.rb_shop -> {
+                binding.rbTime.translationZ = 1f
+                binding.rbGlobal.translationZ = 0f
+                binding.rbShop.translationZ = 2f
+                binding.viewBg.translationZ = 1f
+
+                binding.viewBg.setBackgroundResource(R.drawable.bg_home_shop_hour_sale_bottom_active)
+                binding.timerSeconds.setTextColor(ContextCompat.getColor(requireContext(), R.color.color_007ae6))
+            }
         }
     }
 }
